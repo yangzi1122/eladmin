e6eb0ba2873e399669d78a28d046a73721ee8eab
yangzi1122 <3459669215@qq.com>
2024-04-22 16:12:01 +0800

    feat: 新增项目

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+// 自动导入vue中hook reactive ref等
+import AutoImport from "unplugin-auto-import/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+			//安装两行后你会发现在组件中不用再导入ref，reactive等
+			imports: ['vue', 'vue-router'],
+            //存放的位置
+			dts: "src/auto-import.d.ts",
+		}),
   ],
   resolve: {
     alias: {
